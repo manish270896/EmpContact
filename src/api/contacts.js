1667c456d6f029843eb45d6e3127bb6d13edd96b
manish270896 <42096940+manish270896@users.noreply.github.com>
2021-08-08 20:33:09 +0530

    db server change

--- src/api/contacts.js
+++ src/api/contacts.js
@@ -2,5 +2,6 @@ import axios from 'axios';
 
 export default axios.create({
     //baseURL: "http://localhost:3006/"
-    baseURL: "https://my-json-server.typicode.com/manish270896/server-api"
+    //baseURL: "https://my-json-server.typicode.com/manish270896/server-api/"
+    baseURL: "https://server1-api.herokuapp.com/"
 })
