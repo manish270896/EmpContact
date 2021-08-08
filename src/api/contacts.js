62910100a1cf1f9e01cb51ec8585188fdd346a07
manish270896 <42096940+manish270896@users.noreply.github.com>
2021-08-08 16:14:06 +0530

    server route change

--- src/api/contacts.js
+++ src/api/contacts.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
     //baseURL: "http://localhost:3006/"
-    baseURL: "https://my-json-server.typicode.com/manish270896/server-api/db"
+    baseURL: "https://my-json-server.typicode.com/manish270896/server-api"
 })
